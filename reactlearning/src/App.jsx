4630ee3adf3097eadaac087ff1438064b5453c23
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-05-06 18:53:59 +0545

    making small window sizer tracker

--- reactlearning/src/App.jsx
+++ reactlearning/src/App.jsx
@@ -1,29 +1,11 @@
 import React from "react";
-// import Hello from "./Hello";
-// import reactLogo from "./assets/react.svg"
-// import Bye from "./Bye";
-// import Array from "./Array";
-// import Object from "./Object";
-// import ArrayOfObject from "./ArrayOfObject"
-// import Conditional from "./Conditional";
-// import Props from "./Props";
-// import Button from "./Button";
-// import State from "./State"
-// import Toggle from "./Toggle";
-// import LikeButton from './LikeButton';
-// import UserProfile from "./UserProfile";
-// import Student from "./Student";
-// import InputExample from "./InputExample";
-// import SimpleForm from './SimpleForm';
-// import MultiInputForm from './MultiInputForm';
-// import AdvanceForm from './AdvanceForm';
 
-import Timer from "./Timer";
+import WindowSizeTracker from "./WindowSizeTracker";
 const App = () => {
   return (
     <div>
-      {/* <First /> */}
-      <Timer/>
+ 
+      <WindowSizeTracker/>
     </div>
   );
 };
--- reactlearning/src/WindowSizeTracker.jsx
+++ reactlearning/src/WindowSizeTracker.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react";
 
 const WindowSizeTracker = () => {
+    const [width, setWidth] = useState(window.innerWidth)
 
-    
+  useEffect(()=>{
+    const handleResize = ()=>{
+        setWidth(window.innerWidth)
+    }
+    window.addEventListener("resize",handleResize)
+    return ()=> window.removeEventListener("resize",handleResize)
+
+  },[width])
 
 
   return (
     <div>
       <h2>WindowSizeTracker</h2>
-      <p> current Width : {}</p>
+      <p> current Width <span className=" text-2xl text-red-600"> {width} </span>  px</p>
     </div>
   );
 };
